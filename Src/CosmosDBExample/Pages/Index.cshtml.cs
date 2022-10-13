7381493716f286cdac432f1f2cec60c9c4c9221d
Kyle Bonallo <Kyle.Bonallo@stormid.com>
2022-10-13 12:36:33 +0100

    Initial commit

---
+++ Src/CosmosDBExample/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Microsoft.Azure.Cosmos;
+
+namespace CosmosDBExample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            var cosmosEndpoint = Environment.GetEnvironmentVariable("COSMOS_ENDPOINT");
+            var cosmosKey = Environment.GetEnvironmentVariable("COSMOS_KEY");
+
+            using CosmosClient client = new(
+                accountEndpoint: cosmosEndpoint!,
+                authKeyOrResourceToken: cosmosKey!
+            );
+
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Tests/CosmosTestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CosmosTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
